8034e8bfe867f257a5cc39f8e65dd64bccf1e385
Andrew James Morgan <andyofmelbourne@gmail.com>
2024-10-08 20:56:05 +1100

    add submit_events_proc.sh for facility hit finding

---
+++ offline/submit_events_proc.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# call: ./submit_events.sh <run_no>
+# eg:   ./submit_events.sh 2
+
+source /etc/profile.d/modules.sh
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PARENT_DIR=$(dirname $SCRIPT_DIR)
+source $PARENT_DIR/source_this_at_euxfel
+
+cd $SCRIPT_DIR
+
+sbatch <<EOT
+#!/bin/bash
+
+#SBATCH --array=${1}
+#SBATCH --time=01:00:00
+#SBATCH --export=ALL
+#SBATCH -J events-${EXP_ID}
+#SBATCH -o ${EXP_PREFIX}/scratch/log/events-${EXP_ID}-%A-%a.out
+#SBATCH -e ${EXP_PREFIX}/scratch/log/events-${EXP_ID}-%A-%a.out
+##SBATCH --partition=upex-beamtime
+##SBATCH --reservation=upex_${EXP_ID}
+#SBATCH --partition=upex
+
+source /etc/profile.d/modules.sh
+source $PARENT_DIR/source_this_at_euxfel
+
+set -e
+
+run=\${SLURM_ARRAY_TASK_ID}
+
+#python make_events_file.py \${run} -n 32 
+python make_events_file_proc.py \${run} 
+
+# add pulse energy
+python add_pulsedata.py \${run}
+
+# add is_hit
+#python add_is_hit.py \${run} -t 5 --per_train
+python add_is_hit_proc.py \${run} -t 5 --modules 15
+
+echo events done
+EOT
