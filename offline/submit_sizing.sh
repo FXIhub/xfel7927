405225a291058337936c649c6edaef5cac06e60b
Andrew James Morgan <andyofmelbourne@gmail.com>
2024-10-10 15:34:26 +1100

    tweaks to sizing

--- offline/submit_sizing.sh
+++ offline/submit_sizing.sh
@@ -38,7 +38,7 @@ run=\${SLURM_ARRAY_TASK_ID}
 echo ${1} run = \${run} 
 
 #mpirun -n 32 python add_sizing_cxi.py \${run} --N_sizes 32 --N_angles 32
-python add_sizing_cxi.py \${run} --N_sizes 64 --N_angles 64
+python add_sizing_cxi.py \${run} --N_sizes 64 --N_angles 64 --rmax 0.01 --smin 1e-9 --smax 100e-9 --bins 2
 
 echo sizing done
 EOT
