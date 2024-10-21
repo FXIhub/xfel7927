6c4439b7a00710fc5898b5718a92146628a062e6
Andrew James Morgan <andyofmelbourne@gmail.com>
2024-10-21 13:10:11 +1100

    added phasing scripts

---
+++ offline/3D-phasing.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Launch python code
+start=`date +%s`
+
+# exit on first error
+set -e
+
+cd ${EXP_PREFIX}/scratch/3D-EMC/Ery
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PARENT_DIR=$(dirname $SCRIPT_DIR)
+
+source /etc/profile.d/modules.sh
+source $PARENT_DIR/source_this_at_euxfel
+#source $PARENT_DIR/source_this_at_home
+
+conda activate /home/amorgan/.conda/envs/EMC
+
+# conver emc output to phasing input
+python make_input.py < merged_intensity.pickle --beamstop 32 --cut_edges --soft_edges --size 150 -o intensity_phasing.pickle
+
+# phase intensity_phasing and produce output-phasing.pickle
+phase.py -s -b --no-centre --inversion_symmetry --shrink 1.5 0.4 200 --iters 1000DM 1000ERA 1000DM 1000ERA 1000DM 1000ERA < intensity_phasing.pickle > output-phasing.pickle
+
+end=`date +%s`
+runtime=$((end-start))
+echo 
+echo runtime: "$runtime" seconds
+
+
---
+++ offline/submit_3D-phasing.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# need to run submit_3D-EMC.sh first
+
+# call: ./submit_3D-phasing.sh <recon directory>
+# eg:   ./submit_3D-phasing.sh /gpfs/exfel/exp/SPB/202405/p007927/scratch/2D-EMC/Ery
+
+
+# this is just to get environment variables defined in source_this_at_euxfel
+source /etc/profile.d/modules.sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PARENT_DIR=$(dirname $SCRIPT_DIR)
+source $PARENT_DIR/source_this_at_euxfel
+
+
+sbatch <<EOT
+#!/bin/bash
+
+#SBATCH --time=010:00:00
+#SBATCH --export=ALL
+#SBATCH -J 3DEMC-${EXP_ID}
+#SBATCH -o ${EXP_PREFIX}/scratch/log/3DEMC-${EXP_ID}.out
+#SBATCH -e ${EXP_PREFIX}/scratch/log/3DEMC-${EXP_ID}.out
+##SBATCH --partition=upex-beamtime
+##SBATCH --reservation=upex_${EXP_ID}
+#SBATCH --constraint="A100"
+#SBATCH --partition=allgpu
+
+# exit on first error
+set -e
+
+./3D-phasing.sh
+
+echo 3DEMC done
+EOT
+
+
+
