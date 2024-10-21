0dbfdc6253fb1e56d13e70dc3d82431ec79e6e1f
Andrew James Morgan <andyofmelbourne@gmail.com>
2024-10-21 06:06:05 +0200

    fiddle

--- offline/3D-phasing.sh
+++ offline/3D-phasing.sh
@@ -5,8 +5,6 @@ start=`date +%s`
 # exit on first error
 set -e
 
-cd ${EXP_PREFIX}/scratch/3D-EMC/Ery
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PARENT_DIR=$(dirname $SCRIPT_DIR)
 
@@ -16,11 +14,15 @@ source $PARENT_DIR/source_this_at_euxfel
 
 conda activate /home/amorgan/.conda/envs/EMC
 
+cd ${EXP_PREFIX}/scratch/3D-EMC/Ery
+
 # conver emc output to phasing input
-emc_to_phasing_input.py < merged_intensity.pickle --beamstop 32 --cut_edges --soft_edges --size 150 -o intensity_phasing.pickle
+emc_to_phasing_input.py < merged_intensity.pickle --beamstop 32 --cut_edges --soft_edges --size 300 -o intensity_phasing.pickle
 
 # phase intensity_phasing and produce output-phasing.pickle
-phase.py -s -b --no-centre --inversion_symmetry --shrink 1.5 0.4 200 --iters 1000DM 1000ERA 1000DM 1000ERA 1000DM 1000ERA < intensity_phasing.pickle > output-phasing.pickle
+phase.py -s -b --no-centre --inversion_symmetry --shrink 1.5 0.5 200 --iters 1000DM 1000ERA 1000DM 1000ERA 1000DM 1000ERA < intensity_phasing.pickle > output-phasing.pickle
+
+python save_compressed_phase.py
 
 end=`date +%s`
 runtime=$((end-start))
