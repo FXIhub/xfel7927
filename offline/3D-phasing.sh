5036f811745cc4be68a806df5811967de8a6ec8a
Andrew James Morgan <andyofmelbourne@gmail.com>
2024-10-21 13:13:50 +1100

    bug fix

--- offline/3D-phasing.sh
+++ offline/3D-phasing.sh
@@ -17,7 +17,7 @@ source $PARENT_DIR/source_this_at_euxfel
 conda activate /home/amorgan/.conda/envs/EMC
 
 # conver emc output to phasing input
-python make_input.py < merged_intensity.pickle --beamstop 32 --cut_edges --soft_edges --size 150 -o intensity_phasing.pickle
+emc_to_phasing_input.py < merged_intensity.pickle --beamstop 32 --cut_edges --soft_edges --size 150 -o intensity_phasing.pickle
 
 # phase intensity_phasing and produce output-phasing.pickle
 phase.py -s -b --no-centre --inversion_symmetry --shrink 1.5 0.4 200 --iters 1000DM 1000ERA 1000DM 1000ERA 1000DM 1000ERA < intensity_phasing.pickle > output-phasing.pickle
