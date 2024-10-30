a44c6137aedba4b9dbf0c316626e1e8bd490654e
Andrew James Morgan <andyofmelbourne@gmail.com>
2024-10-30 13:24:33 +1100

    first draft events with per-cell mask

--- offline/make_events_file.py
+++ offline/make_events_file.py
@@ -2,204 +2,255 @@
 add cellID, pulseID, trainID, detector distance to an events file, eg events/events_r0034.h5
 """
 
-# doing this first speeds up running
 import os
 import argparse
 
-PREFIX = os.environ["EXP_PREFIX"]
-
-parser = argparse.ArgumentParser(description='Lit pixel calculator')
-parser.add_argument('run', type=int, help='Run number')
-parser.add_argument('-n', '--nproc', 
-                    help='Number of processes to use',
-                    type=int, default=0)
-parser.add_argument('-o', '--out_folder', 
-                    help='Path of output folder (default=%s/scratch/events/)'%PREFIX,
-                    default=PREFIX+'/scratch/events/')
-parser.add_argument('--out_folder_powder', 
-                    help='Path of output folder (default=%s/scratch/powder/)'%PREFIX,
-                    default=PREFIX+'/scratch/powder/')
-parser.add_argument('-m', '--masks', 
-                    help=f'pixel masks to apply before calculating litpixels and photon counts located in {PREFIX}scratch/det/. Mask must be 1 for good and 0 for bad, and the data must be in /entry_1/good_pixels)',
-                    type=str, nargs='+',
-                    default=['r0065_mask.h5', 'hit_finding_mask.h5'] )
-args = parser.parse_args()
-
-args.masks = [f'{PREFIX}scratch/det/{f}' for f in args.masks]
-
-import sys
-import time
-import glob
+import numpy as np
+import h5py
 import multiprocessing as mp
-import ctypes
+import glob
+from pathlib import Path
+from tqdm import tqdm
+import utils
+import shmemarray
 import subprocess
 
-import h5py
-import numpy as np
+PREFIX = os.environ["EXP_PREFIX"]
+from constants import NMODULES, MODULE_SHAPE
 
-import utils
-from constants import VDS_DATASET, VDS_MASK_DATASET, NMODULES, CHUNK_SIZE, BAD_CELLIDS
-
-
-
-class LitPixels():
-    def __init__(self, vds_file, mask, nproc=0, chunk_size=CHUNK_SIZE, total_intens=False):
-        self.vds_file = vds_file
-        self.chunk_size = chunk_size # Needs to be multiple of 32 for raw data
-        if self.chunk_size % 32 != 0:
-            print('WARNING: Performance is best with a multiple of 32 chunk_size')
-        if nproc == 0:
-            self.nproc = int(subprocess.check_output('nproc').decode().strip())
-        else:
-            self.nproc = nproc
-        print('Using %d processes' % self.nproc)
-
-        with h5py.File(vds_file, 'r') as f:
-            self.dshape = f[VDS_DATASET].shape
-
-        # test
-        #self.frames  = 32 * 1000
-        
-        self.mask    = mask
-        self.frames  = int(self.dshape[0])
-        self.modules = int(self.dshape[1])
-        self.pixels  = int(np.prod(self.dshape[2:]))
-        self.module_shape = self.dshape[2:]
-        self.frame_shape = self.dshape[1:]
-
-    def run_frame(self):
-        sys.stdout.write('Calculating number of lit pixels for all modules for %d frames\n'%(self.frames))
-        sys.stdout.flush()
-        
-        # Litpixels for each module and each frame
-        litpix = mp.Array(ctypes.c_ulong, self.frames)
-        
-        # photon counts for each module and each frame
-        intens = mp.Array(ctypes.c_ulong, self.frames)
-        
-        # powder for each module 
-        powder = mp.Array(ctypes.c_ulong, self.modules * self.pixels)
-        
-        jobs = []
-        for c in range(self.nproc):
-            p = mp.Process(target=self._part_worker, args=(c, litpix, intens, powder))
-            jobs.append(p)
-            p.start()
-        
-        for i, j in enumerate(jobs):
-            sys.stdout.write(f'\n\nwaiting for job {i} to finish... ')
-            sys.stdout.flush()
-            j.join()
-            sys.stdout.write(f'Done\n\n')
-            sys.stdout.flush()
-        
-        self.litpix = np.frombuffer(litpix.get_obj(), dtype='u8').reshape(self.frames)
-        self.intens = np.frombuffer(intens.get_obj(), dtype='u8').reshape(self.frames)
-        self.powder = np.frombuffer(powder.get_obj(), dtype='u8').reshape(self.frame_shape)
-        return self.litpix, self.intens, self.powder
+def get_module_fnams(run, module):
+    # get module file names
+    run_dir = f'{PREFIX}/proc/r{run:>04}/'
+    assert(Path(run_dir).is_dir())
     
-    def _part_worker(self, p, litpix, intens, powder):
-        np_litpix = np.frombuffer(litpix.get_obj(), dtype='u8').reshape(self.frames)
-        np_intens = np.frombuffer(intens.get_obj(), dtype='u8').reshape(self.frames)
-        np_powder = np.frombuffer(powder.get_obj(), dtype='u8').reshape(self.frame_shape)
+    fnams = glob.glob(f'{run_dir}/CORR-*-AGIPD{module:>02}-S*.h5')
+    
+    print(f'found {len(fnams)} files for module {module}.')
+    return fnams
+
+def get_tid_cid_mapping_vds(vds):
+    tc_to_vds_index = {}
+    with h5py.File(vds) as f:
+        tids = f['/entry_1/trainId'][()]
+        cids = f['/entry_1/cellId'][()]
+
+    # initialise lookup 
+    for t in np.unique(tids):
+        tc_to_vds_index[t] = {}
+    
+    for i, (tid, cid) in enumerate(zip(tids, cids)):
+        tc_to_vds_index[tid][cid] = i
+    return tc_to_vds_index
+
+
+class Events():
+    def __init__(self, module, fnams, mask, hit_mask, tc_to_vds_index):
+        self.fnams    = fnams
+        self.module   = module
+        self.mask     = mask
+        self.hit_mask = hit_mask
+        self.tc_to_vds_index = tc_to_vds_index
+         
+        self.data_src = f'/INSTRUMENT/SPB_DET_AGIPD1M-1/CORR/{module}CH0:output/image/data'
         
-        nframes = self.frames
-        my_start = (nframes // self.nproc) * p
-        my_end = min((nframes // self.nproc) * (p+1), nframes)
-        num_chunks = int(np.ceil((my_end-my_start)/self.chunk_size))
-        #num_chunks = 4
-        if p == 0:
-            print('Doing %d chunks of %d frames each' % (num_chunks, self.chunk_size))
-            sys.stdout.flush()
+        N = 0
+        # get total number of events
+        for fnam in fnams:
+            with h5py.File(fnam) as f:
+                N += f[self.data_src].shape[0]
+                self.frame_type = f[self.data_src].dtype
+        
+        self.events = N
+        
+        print(f'found {N} events for module {module}')
 
-        stime = time.time()
-        f_vds = h5py.File(self.vds_file, 'r')
-        for c in range(num_chunks):
-            pmin = my_start + c*self.chunk_size
-            pmax = min(my_start + (c+1) * self.chunk_size, my_end)
+        # check if we can skip hit finding mask
+        if not np.any(hit_mask):
+            self.skip_hit = True
+        else :
+            self.skip_hit = False
+        
+        self.lit      = shmemarray.empty((N,), dtype = np.uint64)
+        self.lit_mask = shmemarray.empty((N,), dtype = np.uint64)
+         
+        self.counts      = shmemarray.empty((N,), dtype = np.uint64)
+        self.counts_mask = shmemarray.empty((N,), dtype = np.uint64)
+        
+        self.cids = shmemarray.empty((N,), dtype = np.uint64)
+        self.tids = shmemarray.empty((N,), dtype = np.uint64)
 
-            # read vds file (assume photon units)
-            mask = f_vds[VDS_MASK_DATASET][pmin:pmax] == 0
-            cids = f_vds['entry_1/cellId'][pmin:pmax]
-            vals = f_vds[VDS_DATASET][pmin:pmax]
-            
-            # mask bad cells and pixels
-            vals[np.isin(cids, BAD_CELLIDS)] = 0
-            vals *= mask
-             
-            t = np.sum(vals, axis=0, dtype=np_powder.dtype)
-            #print(np_powder.dtype, np_powder.shape, vals.dtype, vals.shape, t.dtype, t.shape)
-            np_powder           += t
-             
-            # apply hitfinding mask
-            vals *= self.mask
-            
-            np_litpix[pmin:pmax] = np.sum(vals>0, axis = (1,2,3))
-            np_intens[pmin:pmax] = np.sum(vals, axis = (1,2,3))
-            
-            etime = time.time()
-            if p == 0:
-                sys.stdout.write('\r%.4d/%.4d: %.2f Hz' % (c+1, num_chunks, (c+1)*self.chunk_size/(etime-stime)*self.nproc))
-                sys.stdout.flush()
-        f_vds.close()
-        if p == 0:
-            sys.stdout.write('\n')
-            sys.stdout.flush()
+    def run(self):
+        pool = mp.Pool(len(self.fnams))
+        
+        result_iter = pool.imap_unordered(
+            self.run_worker, range(len(self.fnams))
+        )
+        
+        for r in result_iter:
+            pass
+        
+        self.finish()
+        
+        return self.lit, self.counts, self.lit_mask, self.counts_mask, self.events, self.cids, self.tids
 
-vds_file     = PREFIX+'scratch/vds/r%.4d.cxi' %args.run
-out_fname    = args.out_folder + os.path.splitext(os.path.basename(vds_file))[0] + '_events.h5'
-powder_fname = args.out_folder_powder + os.path.splitext(os.path.basename(vds_file))[0] + '_powder.h5'
-modules   = range(NMODULES)
-
-print(f'reading data from {vds_file}')
-print(f'output file       {out_fname}')
-print(f'powder file       {powder_fname}')
-
-# check that vds file exists
-assert(os.path.exists(vds_file))
-
-# check that vds file points to corrected data
-result = subprocess.run(['h5ls', '-rv', vds_file], stdout=subprocess.PIPE)
-assert('CORR' in result.stdout.decode('utf-8'))
-
-print(f'loading masks {args.masks}')
-if len(args.masks) > 0 :
-    mask = None
-    for fnam in args.masks:
+    def finish(self):
+        # I think this is how we go from shmemarray to np.array
+        self.lit = np.array(self.lit)
+        self.lit_mask = np.array(self.lit_mask)
+        self.counts = np.array(self.counts)
+        self.counts_mask = np.array(self.counts_mask)
+        self.cids = np.array(self.cids)
+        self.tids = np.array(self.tids)
+    
+    def run_worker(self, proc):
+        fnam = self.fnams[proc]
+        
+        if proc == (len(self.fnams)-1) :
+            disable = False
+        else :
+            disable = True
+        
+        data_src    = f'/INSTRUMENT/SPB_DET_AGIPD1M-1/CORR/{self.module}CH0:output/image/data'
+        cellId_src  = f'/INSTRUMENT/SPB_DET_AGIPD1M-1/CORR/{self.module}CH0:output/image/cellId'
+        trainId_src = f'/INSTRUMENT/SPB_DET_AGIPD1M-1/CORR/{self.module}CH0:output/image/trainId'
         with h5py.File(fnam) as f:
-            if mask is None :
-                mask  = f['entry_1/good_pixels'][()]
-            else :
-                mask *= f['entry_1/good_pixels'][()]
-else :
-    mask = 1
+            data = f[data_src]
+            cid  = f[cellId_src][()]
+            tid  = f[trainId_src][()]
+            
+            frame = np.empty(MODULE_SHAPE, dtype = self.frame_type)
+            
+            for d in tqdm(range(data.shape[0]), disable = disable):
+                data.read_direct(frame, np.s_[d], np.s_[:])
+                i = self.tid_cid_mapping_vds[tid[d]][cid[d]]
 
-print('Calculating lit pixels from', vds_file)
-l = LitPixels(vds_file, mask, nproc=args.nproc)
-
-print('Running on the following modules:', modules)
-
-# get number of lit pixels, integrated photon counts and powder for each module
-litpixels, photon_counts, powder = l.run_frame()
-
-# get train, cell and pulse ID for each frame
-print('Copying IDs from VDS file')
-sys.stdout.flush()
-with h5py.File(vds_file, 'r') as f_vds:
-    trainId = f_vds['entry_1/trainId'][:]
-    cellId  = f_vds['entry_1/cellId'][:, 0]
-    pulseId = f_vds['entry_1/pulseId'][:]
-
-with h5py.File(out_fname, 'a') as f:
-    utils.update_h5(f, 'total_intens', np.array(photon_counts), compression=True)
-    utils.update_h5(f, 'litpixels', np.array(litpixels), compression=True)
-    
-    utils.update_h5(f, 'trainId', trainId, compression=True)
-    utils.update_h5(f, 'cellId', cellId, compression=True)
-    utils.update_h5(f, 'pulseId', pulseId, compression=True)
-
-with h5py.File(powder_fname, 'a') as f:
-    utils.update_h5(f, 'powder', np.array(powder), compression=True)
-    
-print('DONE')
+                self.cids[i] = cid[d]
+                self.tids[i] = tid[d]
+                 
+                # apply per-cell mask
+                frame *= self.mask[cid[d]]
                 
+                self.lit[i]    = np.sum(frame>0)
+                self.counts[i] = np.sum(frame)
+                
+                # apply hit_finding mask
+                if self.skip_hit :
+                    self.lit_mask[i]    = 0
+                    self.counts_mask[i] = 0
+                else :
+                    frame *= self.hit_mask
+                    self.lit_mask[i]    = np.sum(frame>0)
+                    self.counts_mask[i] = np.sum(frame)
+        return True
+
+
+def main():
+    PREFIX = os.environ["EXP_PREFIX"]
+    parser = argparse.ArgumentParser(description='Lit pixel calculator')
+    parser.add_argument('run', type=int, help='Run number')
+    parser.add_argument('-o', '--out_folder', 
+                        help='Path of output folder (default=%s/scratch/events/)'%PREFIX,
+                        default=PREFIX+'/scratch/events/')
+    parser.add_argument('--out_folder_powder', 
+                        help='Path of output folder (default=%s/scratch/powder/)'%PREFIX,
+                        default=PREFIX+'/scratch/powder/')
+    parser.add_argument('-m', '--hit_finding_mask', 
+                        help=f'pixel mask to apply before calculating litpixels and photon counts located in {PREFIX}scratch/det/. Mask must be 1 for good and 0 for bad, and the data must be in /entry_1/good_pixels)',
+                        type=str, 
+                        default='hit_finding_mask.h5' )
+    args = parser.parse_args()
+    
+    args.masks = [f'{PREFIX}scratch/det/{f}' for f in args.masks]
+    
+    # get autogenerated per-cell mask
+    args.run_mask = f'{PREFIX}scratch/det/r{args.run:>04}_mask.h5'
+
+    vds_file     = PREFIX+'scratch/vds/r%.4d.cxi' %args.run
+
+    out_fname  = args.out_folder + os.path.splitext(os.path.basename(vds_file))[0] + '_events.h5'
+    
+    # check that run mask file exists
+    assert(os.path.exists(args.run_mask))
+
+    # check that vds file exists
+    assert(os.path.exists(vds_file))
+    
+    # check that vds file points to corrected data
+    result = subprocess.run(['h5ls', '-rv', vds_file], stdout=subprocess.PIPE)
+    assert('CORR' in result.stdout.decode('utf-8'))
+
+    print(f'loading hit finding mask {args.hit_finding_mask}')
+    with h5py.File(args.hit_finding_mask) as f:
+        hit_mask = f['entry_1/good_pixels'][()]
+    
+    # get lookup table for trainId, cellIds -> vds index
+    tid_cid_mapping_vds = get_tid_cid_mapping_vds(vds_file)
+    
+    litpixels     = []
+    photon_counts = []
+    cellIds       = []
+    trainIds      = []
+    modules       = []
+    litpixels_mask     = []
+    photon_counts_mask = []
+    events = []
+
+    
+    for module in range(NMODULES):
+        print(f'loading per-cell mask {args.run_mask} for module {module}')
+        with h5py.File(args.run_mask) as f:
+            cids = f['entry_1/cellIds'][()]
+            mask = {}
+            data = f['entry_1/good_pixels']
+            for i, c in enumerate(cids) :
+                mask[c] = data[i, module]
+        
+        fnams   = get_module_fnams(args.run, module)
+        
+        events = Events(module, fnams, mask, hit_mask[module], tid_cid_mapping_vds)
+        
+        litpix, counts, litpix_mask, counts_mask, N, cids, tids = events.run()
+        
+        litpixels.append(litpix.copy())
+        litpixels_mask.append(litpix_mask.copy())
+        
+        photon_counts.append(counts.copy())
+        photon_counts_mask.append(counts_mask.copy())
+        
+        events.append(N)
+        cellIds.append(cids.copy())
+        trainIds.append(tids.copy())
+        modules.append(module)
+
+    # this is sure to fail, I just want to see when
+
+    # check that we have the same number of events for each module
+    assert(np.allclose(events[0], events))
+    
+    # check that cellIds agree between modules
+    assert(np.allclose(cellIds[0], cellIds))
+
+    # check that train agree between modules
+    assert(np.allclose(trainIds[0], trainIds))
+    
+    # sum counts and litpix over modules
+    litpixels_global     = np.sum(litpixels, axis=0)
+    photon_counts_global = np.sum(photon_counts, axis=0)
+
+    litpixels_mask_global     = np.sum(litpixels_mask, axis=0)
+    photon_counts_mask_global = np.sum(photon_counts_mask, axis=0)
+    
+    print()
+    print(f'writing output to {args.out_fname}')
+    with h5py.File(args.out, 'w') as f:
+        utils.update_h5(f, 'total_intens', photon_counts_global, compression = True, chunks = photon_counts_global.shape)
+        utils.update_h5(f, 'litpixels',    litpixels_global, compression = True, chunks = litpixels_global.shape)
+        utils.update_h5(f, 'total_intens_mask', photon_counts_mask_global, compression = True, chunks = photon_counts_mask_global.shape)
+        utils.update_h5(f, 'litpixels_mask',    litpixels_mask_global, compression = True, chunks = litpixels_mask_global.shape)
+        utils.update_h5(f, 'cellIds', cellIds[0], compression = True)
+        utils.update_h5(f, 'trainIds', trainIds[0], compression = True)
+        utils.update_h5(f, 'modules', np.array(modules), compression = True)
+
+if __name__ == '__main__':
+    main()
+
